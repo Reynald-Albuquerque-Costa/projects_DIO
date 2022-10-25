32e3ba3916cabc1d691d60a71aed10fddd9dd3b3
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-10-24 22:44:16 -0300

    @Reynald-Albuquerque-Costa[Conteúdo das aulas]

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using projects_DIO.Src.Models;
+
+
+// bool exibirMenu = true;
+
+// while(exibirMenu)
+// {
+
+//     Console.WriteLine("Digite sua opção");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     var opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Busca de cliente");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             exibirMenu = false;
+//             break;    
+
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+//     }
+// }
+
+// Console.WriteLine("O prograa encerrou !");
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero = 0;   
+
+// do
+// {
+//     Console.WriteLine("Digite um número para somar (0 para parar)");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// }while(numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+
+
+
+
+
+
+
+
+// int numero = 2;
+// int contador = 0;
+
+// while(contador <=10)
+// {
+//     Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+//     contador++;
+// }
+
+
+
+
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <=10; contador++)
+// {
+//     Console.WriteLine($"{numero} X {contador} = {numero * contador}");
+// }
+
+
+
+
+
+
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 20);
+// calc.Subtrair(20, 10);
+// calc.Multiblicar(5, 5);
+// calc.Dividir(8, 2);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+// calc.RaizQuadrada(9);
+
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Reynald";
+// pessoa1.Idade = 19;
+// pessoa1.Apresentar();
+
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual);
